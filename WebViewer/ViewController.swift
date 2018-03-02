b1d5c3aa85b6d7f273cfedb502f2bbbaaeda530e
Amit Gaikwad <amitgaik@cybage.com>
2018-03-02 10:29:30 +0530

    Initial Commit

---
+++ WebViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewer
+//
+//  Created by Amit Gaikwad on 02/03/18.
+//  Copyright © 2018 Amit Gaikwad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
